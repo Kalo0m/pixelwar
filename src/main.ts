022450252c3883fd56ff569cd4b9b89c24cc2df0
Théo Letouzé <theo.letouze44@gmail.com>
2022-07-09 02:35:30 +0200

    remove unused lines

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 import generatedRoutes from '~pages'
@@ -10,17 +10,9 @@ import './styles/main.css'
 import 'uno.css'
 const routes = setupLayouts(generatedRoutes)
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-})
-router.beforeEach((from, to, next) => {
-  return false
-  next()
-})
 export const createApp = ViteSSG(
   App,
-  router.options,
+  { routes },
   (ctx) => {
     Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
   },
